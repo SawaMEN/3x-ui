a14bb7bd3756c93bd48b97261a9c2a481b613d79
Савватий Менский <sawamensky@yandex.ru>
2026-09-22 22:00:37 +0500

    route new sidecar protocols outside Xray

--- internal/web/service/protocol.go
+++ internal/web/service/protocol.go
@@ -3,7 +3,12 @@ package service
 import "github.com/SawaMEN/3x-ui/v3/internal/database/model"
 
 func isXrayManagedProtocol(protocol model.Protocol) bool {
-	return protocol != model.VKTurnProxy
+	return protocol != model.VKTurnProxy &&
+		protocol != model.MTProto &&
+		protocol != model.AmneziaWG &&
+		protocol != model.TUIC &&
+		protocol != model.Psiphon &&
+		protocol != model.Mieru
 }
 
 func isVKTurnProxyProtocol(protocol model.Protocol) bool {
