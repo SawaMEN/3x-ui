17feca0638da170cb2696d4c6a550193609831d3
Савватий Менский <sawamensky@yandex.ru>
2026-09-22 12:35:45 +0500

    style: wrap telemt regex source constant

--- internal/web/service/telemt_webproxy.go
+++ internal/web/service/telemt_webproxy.go
@@ -29,7 +29,8 @@ const (
 	telemtWebMinEngine  = "3.5.1"
 )
 
-const telemtWebDomainPatternSource = "^(?:[A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?\\.)+[A-Za-z]{2,63}$"
+const telemtWebDomainPatternSource = "^(?:[A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?" +
+	"\\.)+[A-Za-z]{2,63}$"
 const telemtWebVersionPatternSource = "(?i)v?([0-9]+)\\.([0-9]+)\\.([0-9]+)"
 const telemtWebPortOwnerPatternSource = "users:\\(\\(\"([^\"]+)\""
 
