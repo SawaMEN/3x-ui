d972dae4a273da3b3d68836720c3f01609b9bf8a
Савватий Менский <sawamensky@yandex.ru>
2026-09-22 22:37:00 +0500

    improve mieru protocol editor

--- frontend/src/pages/inbounds/form/protocols/mieru.tsx
+++ frontend/src/pages/inbounds/form/protocols/mieru.tsx
@@ -3,28 +3,55 @@ import { InputNumber, Select, Switch } from 'antd';
 
 import { FormField } from '@/components/form/rhf';
 
+const multiplexingOptions = [
+  'MULTIPLEXING_OFF',
+  'MULTIPLEXING_LOW',
+  'MULTIPLEXING_MIDDLE',
+  'MULTIPLEXING_HIGH',
+].map((value) => ({ value, label: value }));
+
+const handshakeOptions = ['HANDSHAKE_STANDARD', 'HANDSHAKE_NO_WAIT'].map((value) => ({
+  value,
+  label: value,
+}));
+
 export default function MieruFields() {
   const { t } = useTranslation();
 
   return (
     <>
-      <FormField name={['settings', 'protocols']} label={t('pages.inbounds.form.mieruProtocols')}>
+      <FormField
+        name={['settings', 'tcpPorts']}
+        label={t('pages.inbounds.form.mieruTcpPorts')}
+        tooltip={t('pages.inbounds.form.mieruPortRangesHint')}
+      >
         <Select
-          mode="multiple"
-          options={[
-            { value: 'TCP', label: 'TCP' },
-            { value: 'UDP', label: 'UDP' },
-          ]}
-          tokenSeparators={[',']}
+          mode="tags"
+          tokenSeparators={[',', ';']}
+          placeholder="2012-2022"
+          style={{ width: '100%' }}
         />
       </FormField>
 
       <FormField
-        name={['settings', 'additionalPorts']}
-        label={t('pages.inbounds.form.mieruAdditionalPorts')}
-        tooltip={t('pages.inbounds.form.mieruAdditionalPortsHint')}
+        name={['settings', 'udpPorts']}
+        label={t('pages.inbounds.form.mieruUdpPorts')}
+        tooltip={t('pages.inbounds.form.mieruPortRangesHint')}
       >
-        <Select mode="tags" tokenSeparators={[',']} />
+        <Select
+          mode="tags"
+          tokenSeparators={[',', ';']}
+          placeholder="2023-2033"
+          style={{ width: '100%' }}
+        />
+      </FormField>
+
+      <FormField name={['settings', 'multiplexing']} label={t('pages.inbounds.form.mieruMultiplexing')}>
+        <Select options={multiplexingOptions} />
+      </FormField>
+
+      <FormField name={['settings', 'handshakeMode']} label={t('pages.inbounds.form.mieruHandshakeMode')}>
+        <Select options={handshakeOptions} />
       </FormField>
 
       <FormField name={['settings', 'mtu']} label={t('pages.inbounds.form.mieruMtu')}>
