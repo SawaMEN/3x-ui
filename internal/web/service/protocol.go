d4a73bb80ee877b788b386532c40230739ac2725
Савватий Менский <sawamensky@yandex.ru>
2026-09-22 22:02:08 +0500

    make NaiveProxy sing-box-only

--- internal/web/service/protocol.go
+++ internal/web/service/protocol.go
@@ -4,6 +4,7 @@ import "github.com/SawaMEN/3x-ui/v3/internal/database/model"
 
 func isXrayManagedProtocol(protocol model.Protocol) bool {
 	return protocol != model.VKTurnProxy &&
+		protocol != model.NaiveProxy &&
 		protocol != model.MTProto &&
 		protocol != model.AmneziaWG &&
 		protocol != model.TUIC &&
