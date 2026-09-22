1939a0796d92dcbd8a473e638222355994436c7e
Савватий Менский <sawamensky@yandex.ru>
2026-09-22 22:50:10 +0500

    psiphon: simplify server settings UI

--- frontend/src/pages/inbounds/form/protocols/psiphon.tsx
+++ frontend/src/pages/inbounds/form/protocols/psiphon.tsx
@@ -1,5 +1,5 @@
 import { useTranslation } from 'react-i18next';
-import { Input, Select } from 'antd';
+import { Input } from 'antd';
 
 import { FormField } from '@/components/form/rhf';
 
@@ -9,39 +9,16 @@ export default function PsiphonFields() {
   return (
     <>
       <FormField
-        name={['settings', 'serverAddress']}
-        label={t('pages.inbounds.form.psiphonServerAddress')}
-        tooltip={t('pages.inbounds.form.psiphonServerAddressHint')}
-      >
-        <Input placeholder="203.0.113.10" />
-      </FormField>
-
-      <FormField
-        name={['settings', 'tunnelProtocol']}
-        label={t('pages.inbounds.form.psiphonTunnelProtocol')}
-      >
-        <Select
-          options={['OSSH', 'SSH', 'TLS-OSSH', 'QUIC-OSSH'].map((value) => ({
-            value,
-            label: value,
-          }))}
-        />
-      </FormField>
-
-      <FormField
         name={['settings', 'serverEntry']}
         label={t('pages.inbounds.form.psiphonServerEntry')}
         tooltip={t('pages.inbounds.form.psiphonServerEntryHint')}
       >
-        <Input.TextArea autoSize={{ minRows: 4, maxRows: 8 }} />
+        <Input.TextArea autoSize={{ minRows: 6, maxRows: 10 }} placeholder="302030203020..." />
       </FormField>
 
-      <FormField
-        name={['settings', 'additionalArguments']}
-        label={t('pages.inbounds.form.psiphonAdditionalArguments')}
-      >
-        <Select mode="tags" tokenSeparators={[' ', ',']} />
-      </FormField>
+      <div style={{ color: 'var(--ant-color-text-secondary)', fontSize: 12, marginTop: 4 }}>
+        {t('pages.inbounds.form.psiphonAutoHint')}
+      </div>
     </>
   );
 }
