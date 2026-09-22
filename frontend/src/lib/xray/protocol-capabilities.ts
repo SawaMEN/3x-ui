3181f04e3e05c3cb210882707db2fca15c1403aa
Савватий Менский <sawamensky@yandex.ru>
2026-09-22 22:07:47 +0500

    fix new protocol sniffing capability predicate

--- frontend/src/lib/xray/protocol-capabilities.ts
+++ frontend/src/lib/xray/protocol-capabilities.ts
@@ -83,7 +83,10 @@ export function canEnableSniffing(values: { protocol: string }): boolean {
     values.protocol !== 'mtproto' &&
     values.protocol !== 'amneziawg' &&
     values.protocol !== 'tuic' &&
-    values.protocol !== 'vk-turn-proxy' && protocol !== 'naive' && protocol !== 'psiphon' && protocol !== 'mieru'
+    values.protocol !== 'vk-turn-proxy' &&
+    values.protocol !== 'naive' &&
+    values.protocol !== 'psiphon' &&
+    values.protocol !== 'mieru'
   );
 }
 
