827245bbbcdad9ccf97346c3fbf66eed904cbfa8
Савватий Менский <sawamensky@yandex.ru>
2026-09-23 09:33:35 +0500

    fix: add protocol network dependencies to all system update targets

--- internal/util/systemupdate/manager_linux.go
+++ internal/util/systemupdate/manager_linux.go
@@ -274,13 +274,13 @@ func requiredPackages(distribution string) []string {
 	case "fedora", "amzn", "rhel", "almalinux", "rocky", "ol", "centos":
 		packages = []string{"cronie", "curl", "tar", "tzdata", "socat", "ca-certificates", "openssl", "util-linux", "iproute", "iptables"}
 	case "arch", "manjaro", "parch":
-		packages = []string{"cronie", "curl", "tar", "tzdata", "socat", "ca-certificates", "openssl", "util-linux"}
+		packages = []string{"cronie", "curl", "tar", "tzdata", "socat", "ca-certificates", "openssl", "util-linux", "iproute2", "iptables"}
 	case "opensuse-tumbleweed", "opensuse-leap":
 		packages = []string{"cron", "curl", "tar", "timezone", "socat", "ca-certificates", "openssl", "util-linux", "iproute2", "iptables"}
 	case "alpine":
-		packages = []string{"dcron", "curl", "tar", "tzdata", "socat", "ca-certificates", "openssl", "util-linux"}
+		packages = []string{"dcron", "curl", "tar", "tzdata", "socat", "ca-certificates", "openssl", "util-linux", "iproute2", "iptables"}
 	default:
-		packages = []string{"cron", "curl", "tar", "tzdata", "socat", "ca-certificates", "openssl", "util-linux"}
+		packages = []string{"cron", "curl", "tar", "tzdata", "socat", "ca-certificates", "openssl", "util-linux", "iproute2", "iptables"}
 	}
 
 	addPackage := func(name string) {
