aed190cc099d96e9b8009e565b4308c4065233bf
Савватий Менский <sawamensky@yandex.ru>
2026-09-26 01:07:03 +0500

    fix: include xz dependency for NaiveProxy archives

--- internal/util/systemupdate/manager_linux.go
+++ internal/util/systemupdate/manager_linux.go
@@ -339,17 +339,17 @@ func requiredPackages(distribution string) []string {
 	var packages []string
 	switch distribution {
 	case "ubuntu", "debian", "armbian":
-		packages = []string{"cron", "curl", "tar", "tzdata", "socat", "ca-certificates", "openssl", "util-linux", "iproute2", "iptables"}
+		packages = []string{"cron", "curl", "tar", "xz-utils", "tzdata", "socat", "ca-certificates", "openssl", "util-linux", "iproute2", "iptables"}
 	case "fedora", "amzn", "rhel", "almalinux", "rocky", "ol", "centos":
-		packages = []string{"cronie", "curl", "tar", "tzdata", "socat", "ca-certificates", "openssl", "util-linux", "iproute", "iptables"}
+		packages = []string{"cronie", "curl", "tar", "xz", "tzdata", "socat", "ca-certificates", "openssl", "util-linux", "iproute", "iptables"}
 	case "arch", "manjaro", "parch":
-		packages = []string{"cronie", "curl", "tar", "tzdata", "socat", "ca-certificates", "openssl", "util-linux", "iproute2", "iptables"}
+		packages = []string{"cronie", "curl", "tar", "xz", "tzdata", "socat", "ca-certificates", "openssl", "util-linux", "iproute2", "iptables"}
 	case "opensuse-tumbleweed", "opensuse-leap":
-		packages = []string{"cron", "curl", "tar", "timezone", "socat", "ca-certificates", "openssl", "util-linux", "iproute2", "iptables"}
+		packages = []string{"cron", "curl", "tar", "xz", "timezone", "socat", "ca-certificates", "openssl", "util-linux", "iproute2", "iptables"}
 	case "alpine":
-		packages = []string{"dcron", "curl", "tar", "tzdata", "socat", "ca-certificates", "openssl", "util-linux", "iproute2", "iptables"}
+		packages = []string{"dcron", "curl", "tar", "xz", "tzdata", "socat", "ca-certificates", "openssl", "util-linux", "iproute2", "iptables"}
 	default:
-		packages = []string{"cron", "curl", "tar", "tzdata", "socat", "ca-certificates", "openssl", "util-linux", "iproute2", "iptables"}
+		packages = []string{"cron", "curl", "tar", "xz", "tzdata", "socat", "ca-certificates", "openssl", "util-linux", "iproute2", "iptables"}
 	}
 
 	addPackage := func(name string) {
