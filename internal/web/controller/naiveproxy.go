b27ad079575b69ea7afba1273f0119770b8b0724
Савватий Менский <sawamensky@yandex.ru>
2026-09-26 01:05:30 +0500

    fix: restrict standalone NaiveProxy to Xray

--- internal/web/controller/naiveproxy.go
+++ internal/web/controller/naiveproxy.go
@@ -1,11 +1,16 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/SawaMEN/3x-ui/v3/internal/naiveproxy"
+	"github.com/SawaMEN/3x-ui/v3/internal/web/service"
 	"github.com/gin-gonic/gin"
 )
 
-type NaiveProxyController struct{}
+type NaiveProxyController struct {
+	settingService service.SettingService
+}
 
 func NewNaiveProxyController(g *gin.RouterGroup) *NaiveProxyController {
 	a := &NaiveProxyController{}
@@ -15,12 +20,31 @@ func NewNaiveProxyController(g *gin.RouterGroup) *NaiveProxyController {
 	return a
 }
 
+func (a *NaiveProxyController) standaloneAllowed() error {
+	coreType, err := a.settingService.GetCoreType()
+	if err != nil {
+		return err
+	}
+	if coreType != service.CoreTypeXray {
+		return fmt.Errorf("standalone NaiveProxy is available only when Xray is selected")
+	}
+	return nil
+}
+
 func (a *NaiveProxyController) status(c *gin.Context) {
+	if err := a.standaloneAllowed(); err != nil {
+		jsonObj(c, naiveproxy.Status{}, err)
+		return
+	}
 	status, err := naiveproxy.GetStatus(c.Request.Context())
 	jsonObj(c, status, err)
 }
 
 func (a *NaiveProxyController) update(c *gin.Context) {
+	if err := a.standaloneAllowed(); err != nil {
+		jsonObj(c, naiveproxy.Status{}, err)
+		return
+	}
 	status, err := naiveproxy.Update(c.Request.Context())
 	jsonObj(c, status, err)
 }
